5e5890186d20ea83dcfe6dbdc03d8c5371e58cdd
samnangcattor <kcattor@gmail.com>
2016-10-04 10:15:12 +0700

    fix bug

--- app/services/tool.rb
+++ app/services/tool.rb
@@ -2,7 +2,7 @@ class Tool
   class << self
     def get_list_files service
       result = []
-      list_files = service.list_files corpus: "domain"
+      list_files = service.list_files corpus: "domain", max_results: 1000, q: "mimeType != 'application/vnd.google-apps.folder'"
       list_files.items.each do|file|
         unless file.mime_type.include?("application/vnd.google-apps.folder")
           result << {"title": file.title, "alert_link": file.alternate_link}
